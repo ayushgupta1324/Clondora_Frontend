45720f8510ffaeec8d902299c8165cab3b3dfe4e
ayushgupta1324 <aushgupta1324@gmail.com>
2024-03-16 23:38:30 +0530

    Minour UI Updates in Skeleton and Navbar

--- src/components/SearchModal/index.jsx
+++ src/components/SearchModal/index.jsx
@@ -1,11 +1,20 @@
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 
 const SearchModal = () => {
   console.log("first");
 
-  return <div>
-    <input placeholder="Search products"/>
-  </div>;
+  return (
+    <div className="search-container">
+      <InputGroup className="input-group">
+        <Input placeholder="Search Products" />
+        <InputRightElement>
+          <AiOutlineSearch />
+        </InputRightElement>
+      </InputGroup>
+    </div>
+  );
 };
 
 export default SearchModal;
